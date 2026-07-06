31617bedf76910e192270b2a1a1bab4ef74bf46a
mkiczorowski <milosz.kiczorowski@softwareone.com>
2026-07-06 14:23:13 +0200

    added sq; db connection

--- src/backend/Program.cs
+++ src/backend/Program.cs
@@ -1,9 +1,14 @@
 using IdentityServer.Configuration;
+using IdentityServer.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.Configure<JwtSettings>(
     builder.Configuration.GetSection(JwtSettings.SectionName));
